b32293ff1a1c01e327528c79d2e354d651c0e167
sakibsarker <sakib15-3824@diu.edu.bd>
2023-03-24 15:03:35 +0600

    spiling fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <>
     <Text fontSize={12} marginTop={10}>
-        Copyright 2023 Poweard by sakib
+        Copyright 2023 Powered by sakib
     </Text>
     </>
   )
